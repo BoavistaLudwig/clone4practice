76c430454610cbd7d77f1b6062c740bae5a9f3b1
BoavistaLudwig <95169251+BoavistaLudwig@users.noreply.github.com>
2022-04-14 11:52:01 +0100

    First js done

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const clock = document.getElementById(".clock");
+const clock = document.querySelector(".clock");
 const api_time_url = "https://worldtimeapi.org/api/timezone/Europe/Lisbon";
+const weather = document.querySelector(".weather");
+const api_weather_url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=41.15&lon=-8.61024&appid=9f3e12bb5cf04b50d55963aba3526d3d";
 
-getData();
+getTime();
+getWeather();
 
-async function getData() {
+async function getTime() {
   const response = await fetch(api_time_url);
   const data = await response.json();
   const date = data.datetime;
   const hour = date.slice(11, 16);
 
   clock.innerHTML = hour;
+}
+
+async function getWeather() {
+  const response = await fetch(api_weather_url);
+  const data = await response.json();
+
+  console.log(data);
 }
